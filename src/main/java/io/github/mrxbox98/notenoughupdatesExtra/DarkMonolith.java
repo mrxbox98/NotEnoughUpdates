62029e346f5281345ee1e4cd69e5710feda0059f
mrxbox98 <saahil.thoppay@gmail.com>
2021-02-03 10:51:23 -0800

    Working on Block Highlight

--- src/main/java/io/github/mrxbox98/notenoughupdatesExtra/DarkMonolith.java
+++ src/main/java/io/github/mrxbox98/notenoughupdatesExtra/DarkMonolith.java
@@ -2,13 +2,16 @@ package io.github.mrxbox98.notenoughupdatesExtra;
 
 import io.github.moulberry.notenoughupdates.miscfeatures.CustomItemEffects;
 import net.minecraft.block.BlockDragonEgg;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.client.model.animation.Animation;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.util.BlockSnapshot;
 import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
@@ -31,16 +34,19 @@ public class DarkMonolith {
         eggs.add(new BlockPos(49,202,-162));
     }
 
-    public void checkBlocks()
+    public BlockPos checkBlocks()
     {
         for(int i=0;i<eggs.size();i++)
         {
             if(Minecraft.getMinecraft().theWorld.getBlockState(eggs.get(i)) instanceof BlockDragonEgg)
             {
-                currBlock = eggs.get(i);
                 Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText("§aDark Monolith at " + eggs.get(i).toString()));
+
+                return eggs.get(i);
+
             }
         }
+        return null;
     }
 
     public static void register()
@@ -50,12 +56,18 @@ public class DarkMonolith {
 
 
 
+
+
     @SubscribeEvent
     public void onWorldRenderLast(RenderWorldLastEvent event)
     {
-        System.out.println("XBOX CODE:" + "onWorldRenderLast(RenderWorldLastEvent event) WORKED!!");
+        //System.out.println("XBOX CODE:" + "onWorldRenderLast(RenderWorldLastEvent event) WORKED!!");
+        BlockPos temp = checkBlocks();
+        if(temp!=null)
+        {
+            drawBlock(temp,event.partialTicks);
+        }
 
-        drawBlock(new BlockPos(100,100,100),event.partialTicks);
 
     }
 
@@ -82,10 +94,13 @@ public class DarkMonolith {
         drawTracerLine(px+1D, py+1.0D, pz+1, px, py+1.0D, pz+1, 1F, 1F, 1F, 0.45F, 5F);
         drawTracerLine(px+1D, py, pz+1, px, py, pz+1, 1F, 1F, 1F, 0.45F, 5F);
         drawTracerLine(px+1D, py+1D, pz, px+1D, py+1D, pz+1, 1F, 1F, 1F, 0.45F, 5F);
+        drawTracerLine(px+1D, py, pz, px+1D, py, pz+1, 1F, 1F, 1F, 0.45F, 5F);
+        drawTracerLine(px, py+1D, pz, px, py+1D, pz+1, 1F, 1F, 1F, 0.45F, 5F);
+        drawTracerLine(px, py, pz, px, py, pz+1, 1F, 1F, 1F, 0.45F, 5F);
 
     }
 
-    public static void drawTracerLine(double x, double y, double z, double x2, double y2, double z2, float red, float green, float blue, float alpha, float lineWdith) {
+    public static void drawTracerLine(double x, double y, double z, double x2, double y2, double z2, float red, float green, float blue, float alpha, float lineWidth) {
         GL11.glPushMatrix();
         GL11.glEnable(GL11.GL_BLEND);
         GL11.glEnable(GL11.GL_LINE_SMOOTH);
@@ -94,7 +109,7 @@ public class DarkMonolith {
         GL11.glDisable(GL11.GL_TEXTURE_2D);
         GL11.glBlendFunc(770, 771);
         GL11.glEnable(GL11.GL_BLEND);
-        GL11.glLineWidth(lineWdith);
+        GL11.glLineWidth(lineWidth);
         GL11.glColor4f(red, green, blue, alpha);
         GL11.glBegin(2);
         GL11.glVertex3d(x2, y2, z2);
