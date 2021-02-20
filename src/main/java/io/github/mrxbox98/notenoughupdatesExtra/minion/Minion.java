13b0d87c8121dcaf2fe3c929c4431cd4f78d3646
mrxbox98 <saahil.thoppay@gmail.com>
2021-02-20 11:50:17 -0800

    Update Minion.java

--- src/main/java/io/github/mrxbox98/notenoughupdatesExtra/minion/Minion.java
+++ src/main/java/io/github/mrxbox98/notenoughupdatesExtra/minion/Minion.java
@@ -10,7 +10,22 @@ public class Minion {
 
     static ArrayList<Minion> minions = new ArrayList<Minion>();
 
-    static final Minion COBBLESTONE_1=new Minion(128,)
+
+    //-------------------------------------------------------
+    //                   Minion Registry
+    //-------------------------------------------------------
+    static final Minion COBBLESTONE_1=new Minion(128,128,"Cobblestone Minion I");
+    static final Minion COBBLESTONE_2=new Minion(128,128,"Cobblestone Minion II");
+    static final Minion COBBLESTONE_3=new Minion(150,150,"Cobblestone Minion III");
+    static final Minion COBBLESTONE_4=new Minion(150,150,"Cobblestone Minion IV");
+    static final Minion COBBLESTONE_5=new Minion(180,180,"Cobblestone Minion V");
+    static final Minion COBBLESTONE_6=new Minion(180,180,"Cobblestone Minion VI");
+    static final Minion COBBLESTONE_7=new Minion(200,200,"Cobblestone Minion VII");
+    static final Minion COBBLESTONE_8=new Minion(200,200,"Cobblestone Minion VIII");
+    static final Minion COBBLESTONE_9=new Minion(225,225,"Cobblestone Minion IX");
+    static final Minion COBBLESTONE_10=new Minion(225,225,"Cobblestone Minion X");
+    static final Minion COBBLESTONE_11=new Minion(257,257,"Cobblestone Minion XI");
+
 
     private String name;
 
